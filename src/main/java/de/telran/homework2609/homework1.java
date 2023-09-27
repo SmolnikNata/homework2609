90c76824b55cc1821dabbd936fe0e53551447d79
Natalia Smolnikova <ntnatali@gmail.com>
2023-09-27 20:58:52 +0200

    homework1

--- src/main/java/de/telran/homework2609/homework1.java
+++ src/main/java/de/telran/homework2609/homework1.java
@@ -2,7 +2,18 @@ package de.telran.homework2609;
 
 public class homework1 {
     public static void main(String[] args) {
-        System.out.println("ntrcn");
+char OL  = 'g';
+byte NA = 4;
+short SP = 56;
+float KL = 4.7355346F;
+double Double = 4.3554353532;
+long Long = 12_121;
+        System.out.println("OL = "+ OL);
+        System.out.println("NA = " + NA);
+        System.out.println("SP = " + SP);
+        System.out.println("KL = " + KL);
+        System.out.println("Double = " + Double);
+        System.out.println("Long = " + Long);
     }
 }
 
@@ -12,5 +23,13 @@ public class homework1 {
 
 
 
-
+//В методе main инициализировать все примитивные типы.
+//Вывести их результат в консоль:
+//char: G
+//int: 89
+//byte: 4
+//short: 56
+//float: 4.7333436
+//double: 4.355453532
+//long: 12121
 
